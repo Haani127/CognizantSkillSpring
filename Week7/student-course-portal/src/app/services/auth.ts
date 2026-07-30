e9f0ee94ee6e9fe5a36aa7b87c753bc11b352bd9
yourusername <mohammedhaani2011@gmail.com>
2026-07-30 11:56:57 +0530

    Remove Week7 directory

--- Week7/student-course-portal/src/app/services/auth.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@angular/core';
-
-// Step 75: AuthService — single source of truth for authentication state
-@Injectable({ providedIn: 'root' })
-export class AuthService {
-  isLoggedIn = true;
-}
